25366a89268f776d88b91bcd8ceab12fb59154f7
Jamblefoot <32472250+Jamblefoot@users.noreply.github.com>
2024-12-16 20:18:41 -0800

    added health component to player and enemy
    it's not wired up yet, nothing dealing damage yet

---
+++ Scenes/Components/health_component.gd
@@ -0,0 +1,32 @@
+extends Node
+
+signal health_changed(new_health: float)
+signal damaged(amount: float)
+signal healed(amount: float)
+signal died
+
+@export var max_health: float = 100
+
+var health: float
+
+
+func _ready():
+	health = max_health
+
+
+func take_damage(damage: float):
+	health -= damage
+	if damage < 0: #healing
+		healed.emit(-damage)
+		if health > max_health:
+			health = max_health
+			#TODO if you wanna overcharge health, do it here
+	elif damage > 0:
+		damaged.emit(damage)
+		if health <= 0:
+			health = 0
+			died.emit()
+	else:
+		print(get_parent().name + " JUST GOT DAMAGED FOR 0. weird.")
+		
+	health_changed.emit(health)

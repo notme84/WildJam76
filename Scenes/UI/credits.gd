144f954570b03c0007ff20776003faa4a876f845
Jamblefoot <32472250+Jamblefoot@users.noreply.github.com>
2024-12-18 09:02:17 -0800

    added credits scene, main menu refactor

---
+++ Scenes/UI/credits.gd
@@ -0,0 +1,17 @@
+extends CanvasLayer
+
+signal closing
+
+
+func _ready():
+	%BackButton.pressed.connect(on_quit)
+
+
+func _input(event: InputEvent):
+	if event.is_action_pressed("ui_cancel") || event.is_action_pressed("back"):
+		on_quit()
+
+
+func on_quit():
+	closing.emit()
+	queue_free()
--- Scenes/main_menu.gd
+++ Scenes/main_menu.gd
@@ -2,11 +2,13 @@ extends Node
 
 @export var game_scene: PackedScene
 @export var options_scene: PackedScene
+@export var credits_scene: PackedScene
 
 
 func _ready():
 	%StartButton.pressed.connect(on_start)
-	%OptionsButton.pressed.connect(on_options_pressed)
+	%OptionsButton.pressed.connect(open_submenu.bind(options_scene, %OptionsButton))
+	%CreditsButton.pressed.connect(open_submenu.bind(credits_scene, %CreditsButton))
 	%QuitButton.pressed.connect(on_quit)
 	
 	%StartButton.grab_focus()
@@ -16,14 +18,24 @@ func on_start():
 	get_tree().change_scene_to_packed(game_scene)
 
 
-func on_options_pressed():
-	var options = options_scene.instantiate()
-	add_child(options)
-	options.closing.connect(on_options_closed)
+func open_submenu(scene: PackedScene, button: Button):
+	var submenu = scene.instantiate()
+	add_child(submenu)
+	submenu.closing.connect(focus_on_button.bind(button))
 
 
-func on_options_closed():
-	%OptionsButton.grab_focus()
+func focus_on_button(button: Button):
+	button.grab_focus()
+
+
+#func on_options_pressed():
+	#var options = options_scene.instantiate()
+	#add_child(options)
+	#options.closing.connect(on_options_closed)
+#
+#
+#func on_options_closed():
+	#%OptionsButton.grab_focus()
 
 
 func on_quit():

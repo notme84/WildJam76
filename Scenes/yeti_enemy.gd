fd7234e5756376b534c2c7f0ae9f0828fb8f18ec
notme84 <nguster@gmail.com>
2024-12-20 12:26:33 -0800

    enemy hitbox

---
+++ Scenes/yeti_enemy.gd
@@ -0,0 +1,10 @@
+extends CharacterBody3D
+@onready var health_component: HealthComponent = $HealthComponent
+
+
+func _ready():
+	health_component.died.connect(on_died)
+
+
+func on_died():
+	queue_free()
